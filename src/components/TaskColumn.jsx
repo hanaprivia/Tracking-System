125baa02ea78aa72183dc2235c74bea973891921
Thi Minh Ha Nguyen <hanaprivia@192-168-1-109.tpgi.com.au>
2024-11-09 17:38:54 +1100

    update entire project

--- src/components/TaskColumn.jsx
+++ src/components/TaskColumn.jsx
@@ -1,17 +1,30 @@
 import React from "react";
+import Todo from "../assets/direct-hit.png";
 
 import "./TaskColumn.css";
-import TaskCard from "./TaskCard.css";
+import TaskCard from "./TaskCard";
 
-const TaskColumn = ({ title, icon }) => {
-  return (
-    <section className="task_column">
-      <h2 className="task_column_heading">
-        <img className="task_column_icon" src={icon} alt="" /> {title}
-      </h2>
-      <TaskCard title="This is Sample Text" tags={["HTML", "CSS"]} />
-    </section>
-  );
+const TaskColumn = ({ title, icon, tasks, status, handleDelete }) => {
+    return (
+        <section className='task_column'>
+            <h2 className='task_column_heading'>
+                <img className='task_column_icon' src={icon} alt='' /> {title}
+            </h2>
+
+            {tasks.map(
+                (task, index) =>
+                    task.status === status && (
+                        <TaskCard
+                            key={index}
+                            title={task.task}
+                            tags={task.tags}
+                            handleDelete={handleDelete}
+                            index={index}
+                        />
+                    )
+            )}
+        </section>
+    );
 };
 
 export default TaskColumn;
